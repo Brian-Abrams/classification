119a7d108efe3c6dc68c11182c6ef29a05914efb
Jeff Ma <jeffrey.ma95@gmail.com>
2018-04-30 03:44:21 -0400

    MIRA done

--- mira.py
+++ mira.py
@@ -20,14 +20,14 @@ class MiraClassifier:
   def __init__( self, legalLabels, max_iterations):
     self.legalLabels = legalLabels
     self.type = "mira"
-    self.automaticTuning = False 
+    self.automaticTuning = False
     self.C = 0.001
     self.legalLabels = legalLabels
     self.max_iterations = max_iterations
     self.initializeWeightsToZero()
 
   def initializeWeightsToZero(self):
-    "Resets the weights of each label to zero vectors" 
+    "Resets the weights of each label to zero vectors"
     self.weights = {}
     for label in self.legalLabels:
       self.weights[label] = util.Counter() # this is the data-structure you should use
@@ -46,31 +46,55 @@ class MiraClassifier:
 
   def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, Cgrid):
     """
-    This method sets self.weights using MIRA.  Train the classifier for each value of C in Cgrid, 
+    This method sets self.weights using MIRA.  Train the classifier for each value of C in Cgrid,
     then store the weights that give the best accuracy on the validationData.
-    
-    Use the provided self.weights[label] data structure so that 
+
+    Use the provided self.weights[label] data structure so that
     the classify method works correctly. Also, recall that a
     datum is a counter from features to values for those features
     representing a vector of values.
     """
+    scores = util.Counter()
+    cweight = util.Counter()
+    count = util.Counter()
     for c in Cgrid:
-        for iteration in range(self.max_iterations):
-          print "Starting iteration ", iteration+1, "..."
-          for i in range(len(trainingData)):
-              self.features = trainingData[i].keys()
-              scores = util.Counter()
+      hits = 0
+      for iteration in range(self.max_iterations):
 
-              for j in range(len(self.weights)):
-                scores[j] = self.weights[j] * trainingData[i]    #dot product is a sum w * f
-              guess = scores.argMax()
+        print "Starting iteration " + str(iteration+1) + "..."
 
-              if guess == trainingLabels[i]:
-                continue                            #guessed right
-              normvector = trainingData[i]
-              normvector.normalize()
-              self.weights[guess] -= normvector              #decrease weight of guess
-              self.weights[trainingLabels[i]] += normvector     #increase weight of correct
+        for i in range(len(trainingData)):
+          for j in self.legalLabels:
+            scores[j] = trainingData[i] * self.weights[j]
+
+          guess = scores.argMax()
+
+          if not (trainingLabels[i] == guess):
+            a = trainingLabels[i]
+            b = trainingData[i]
+            t = ( ( (self.weights[guess] - self.weights[a])*b + 1.0 )/(2.0*(b*b)) )
+            maxt = min(c, t)
+            varvector = util.Counter()
+            for k in trainingData[i].keys():
+              varvector[k] = trainingData[i][k] * maxt
+
+            self.weights[trainingLabels[i]] += varvector
+            self.weights[scores.argMax()] -= varvector
+
+      cweight[c] = self.weights
+
+      # Now run this against the validation data to see which c is the best
+      for i in range(len(validationData)):
+        for j in validationLabels:
+          scores[j] = validationData[i] * self.weights[j]
+
+        if validationLabels[i] == scores.argMax():
+          hits += 1  # Add a count to each hit
+
+      count[c] = hits  # Add the amount of hits to the respective c
+
+    print "Using weight number " + str(count.argMax())
+    self.weights = cweight[count.argMax()]  # Use the highest hit rate one
 
   def classify(self, data ):
     """
@@ -87,7 +111,6 @@ class MiraClassifier:
       guesses.append(vectors.argMax())
     return guesses
 
-  
   def findHighOddsFeatures(self, label1, label2):
     """
     Returns a list of the 100 features with the greatest difference in feature values
